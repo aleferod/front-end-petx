c393846c49de83e85eba02457a2659eba6f4c421
Alefe Oliveira <alefe.oliveira@verity.com.br>
2020-08-26 18:27:18 -0300

    Implementando te de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { ForgotMyPasswordComponent } from './forgot-my-password/forgot-my-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    ForgotMyPasswordComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/forgot-my-password/forgot-my-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-my-password',
+  templateUrl: './forgot-my-password.component.html',
+  styleUrls: ['./forgot-my-password.component.css']
+})
+export class ForgotMyPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
