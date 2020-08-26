4927f644de3f3209ed7998587dad7c9405fcdbcd
Alefe Oliveira <alefe.oliveira@verity.com.br>
2020-08-26 18:52:37 -0300

    Implementando tela de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/login/login.component'
-
+import {CreateAccountComponent} from '../app/create-account/create-account.component'
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'create-account', component: CreateAccountComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { ForgotMyPasswordComponent } from './forgot-my-password/forgot-my-password.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CreateAccountComponent,
-    ForgotMyPasswordComponent
+    ForgotMyPasswordComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
